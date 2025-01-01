8281a88e9a7a82531cf05da2ff7253cf164f6d2c
edard3v <edard3v@gmail.com>
2025-01-01 06:32:58 -0500

    agg btn

---
+++ src/components/buttons/Btn/Btn.tsx
@@ -0,0 +1,23 @@
+import Cross from "@components/icons/Cross";
+import css from "./Btn.module.css";
+import { cls } from "@utils/cls";
+import { ButtonHTMLAttributes } from "react";
+
+export default function Btn(props: Props) {
+  const { className, children, loading, err, disabled, ...rest } = props;
+
+  return (
+    <button {...rest} className={cls([css.btn, className])} disabled={disabled}>
+      {!loading && children}
+
+      {loading && <div className={css.loading}></div>}
+
+      {err && !loading && !disabled && <Cross className={css.err} />}
+    </button>
+  );
+}
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  loading?: boolean;
+  err?: boolean;
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import Layout from "@layouts/Layout/Layout";
 import { HOME } from "./config";
+import Btn from "@components/buttons/Btn/Btn";
 
 export default function Home() {
   return (
     <Layout title={HOME.display}>
       <main>
         <h1>¡Hola mundo!</h1>
+        <Btn>Enviar</Btn>
       </main>
     </Layout>
   );
