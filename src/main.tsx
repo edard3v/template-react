c3a41216e10bf41306a3fe17627347c67d47a46c
edard3v <edard3v@gmail.com>
2025-02-16 11:31:24 -0500

    agg tanStackQuery y zustand

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,22 @@ import "./main.css";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // default: true,
+      staleTime: 24 * 60 * 60 * 1000, // 24h,
+      retry: 1,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+    <ReactQueryDevtools />
+  </QueryClientProvider>
 );
